e900cf8ae1dd48ec1b394c35708beb20d74a5f40
Yakubu Lute <yakubulute@outlook.com>
2025-02-16 00:12:43 -1200

    Updates

--- events.io-frontend/src/assets/images/index.ts
+++
@@ -1,4 +0,0 @@
-export const defaultUserPicture = '/assets/images/user-default-image.png'
-export const emptySearchSvg = '/assets/svgs/no_requests.svg'
-export const profilePictureSVGCircle = '/assets/images/user-default-image-cir.svg'
-export const profilePictureSVGSquare = '/assets/images/user-default-image-cir.svg'
