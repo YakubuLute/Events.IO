7511473f0ca24dc83ada3c25b880fe8278808588
Yakubu Lute <yakubulute@outlook.com>
2025-02-21 09:29:19 -1200

    Pulled

--- events.io-frontend/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import HomePage from '@/components/landing'
-export default function Home () {
-  return (
-    <div className=''>
-      <HomePage />
-    </div>
-  )
-}
--- events.io-frontend/src/utils/truncateAddress.ts
+++
@@ -1,4 +0,0 @@
-export const truncateAddress = (address: string, truncationLength = 6) => {
-	const truncatedAddress = address?.slice(0, truncationLength) + "..." + address?.slice(-truncationLength);
-	return truncatedAddress;
-}
