9392731dac9d2c2ceb2e66768a4f348f8855fc3e
Yakubu Lute <yakubulute@outlook.com>
2025-02-25 14:12:04 -1200

    Fixed mongoDb connection issues and tailwind issues

--- events.io/src/app/layout.tsx
+++ events.io/src/app/layout.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Metadata } from 'next'
-// import { Geist, Geist_Mono } from 'next/font/google'
 import { Notifications } from '@mantine/notifications'
-
+import './globals.css'
 import '@mantine/core/styles.css'
 
 import React from 'react'
@@ -20,7 +19,6 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout ({ children }: { children: any }) {
-  
   return (
     <html lang='en' {...mantineHtmlProps}>
       <head>
